99439f9e11357549843c205ae82aa97caf8cdbe0
Rob McDougall <Rob7McDougall@hotmail.comgit config --global user.name RobAMcDougallqssh-keygenuitquit>
2024-02-19 15:57:51 +0000

    add song list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Intro from "./components/Intro.jsx";
+import Songlist from "./components/Songlist.jsx";
 import {useState} from "react";
 
 export default function App() {
     return(
-    <Intro />
+     <> < Intro /> <Songlist/> </>
     )
 }
---
+++ src/components/Songlist.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+export default function Songlist() {
+    return (
+        <div class="songList">
+            <div>
+                <h1>Albums:</h1>
+                <h2>Hot Fuss</h2>
+                <p>June 15th 2004</p>
+                <img src="src/assets/The_Killers_-_Hot_Fuss.png" alt="" />
+            </div>
+            <div>
+                <h2>Sam's Town</h2>
+                <p>October 3rd 2006</p>
+                <img src="src/assets/Sam's_Town.jpeg" alt="" />
+            </div>
+            <div>
+                <h2>Day And Age</h2>
+                <p>November 18th 2008</p>
+                <img src="src/assets/The_Killers_-_Day_&_Age.png" alt="" />
+            </div>               
+        </div>
+    )
+}
